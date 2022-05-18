9bd6647d04cae2409a0d5bb50d38bccd2e07b728
varun <vgpolampalli@gmail.com>
2022-05-18 18:13:43 +0530

    done

--- src/main/java/vgp/App.java
+++ src/main/java/vgp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello ratha1213 123 " );
+        System.out.println( "Hello varun " );
     }
 }
